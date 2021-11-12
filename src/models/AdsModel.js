3c72c447909472a1a450766d7fe9e62d36a08dbf
Javohir <javohir200903@gmail.com>
2021-11-13 01:37:21 +0500

    add AdsModel.js

---
+++ src/models/AdsModel.js
@@ -0,0 +1,29 @@
+module.exports = async (sequelize, Sequelize) => {
+    return await sequelize.define("ads", {
+        ads_id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        ads_title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        ads_phone: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        ads_description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        ads_slug: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        ads_photo: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+    });
+};
