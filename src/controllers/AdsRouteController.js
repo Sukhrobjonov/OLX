a9fa4f93cee4f18513dd3149dcab7f6e3d8ec113
Javohir <javohir200903@gmail.com>
2021-11-13 06:18:58 +0500

    fix include

--- src/controllers/AdsRouteController.js
+++ src/controllers/AdsRouteController.js
@@ -1,10 +1,105 @@
+const { AddAdsValidation } = require("../modules/validations");
+const path = require("path");
+const { default: slugify } = require("slugify");
+
 module.exports = class AdsRouteController {
-    static async AdsGetController(req, res) {
+    static async AdsAddGetController(req, res) {
         try {
             res.render("add_ads", {
                 user: req.user,
                 role: req.role,
                 categories: await req.db.categories.findAll({ raw: true }),
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async AdsAddPostController(req, res) {
+        try {
+            const data = await AddAdsValidation(req.body);
+            const photo = req?.files?.photo;
+
+            const slug =
+                slugify(data.title, {
+                    lower: true,
+                    strict: true,
+                    replacement: "_",
+                }) + Date.now();
+
+            if (photo) {
+                if (photo?.size > 5 * 1024 * 1024)
+                    throw new Error("Rasimni hajimi 5 mb dan kamroq bo'lishi");
+
+                let photo_name =
+                    photo.md5 +
+                    "." +
+                    photo.mimetype.split("/")[
+                        photo.mimetype.split("/").length - 1
+                    ];
+
+                photo.mv(
+                    path.join(__dirname, "..", "public", "img", photo_name)
+                );
+
+                const ad = await req.db.ads.create({
+                    ads_title: data.title,
+                    ads_phone: data.phone,
+                    ads_price: data.price,
+                    ads_description: data.description,
+                    ads_slug: slug,
+                    ads_photo: photo_name,
+                    user_id: req.user.user_id,
+                    category_id: data.category,
+                });
+
+                res.redirect("/ads/" + slug);
+
+                console.log(ad.dataValues);
+            }
+
+            const ad = await req.db.ads.create({
+                ads_title: data.title,
+                ads_phone: data.phone,
+                ads_price: data.price,
+                ads_description: data.description,
+                ads_slug: slug,
+                user_id: req.user.user_id,
+                category_id: data.category,
+            });
+            console.log(ad.dataValues);
+
+            res.redirect("/ads/" + slug);
+        } catch (error) {
+            console.log(error);
+            res.render("add_ads", {
+                error,
+            });
+        }
+    }
+
+    static async AdsGetOneController(req, res) {
+        try {
+            const ads = await req.db.ads.findOne({
+                where: { ads_slug: req.params.slug },
+                raw: true,
+                include: [
+                    {
+                        model: req.db.users,
+                        attributes: ["user_name", "user_email"],
+                    },
+                    {
+                        model: req.db.categories,
+                        attributes: ["category_name"],
+                    },
+                ],
+            });
+
+            console.log(ads);
+            res.render("ads_page", {
+                user: req.user,
+                role: req.role,
+                ads,
             });
         } catch (error) {
             console.log(error);
--- src/routes/AdsRoute.js
+++ src/routes/AdsRoute.js
@@ -1,8 +1,15 @@
-const { AdsGetController } = require("../controllers/AdsRouteController");
+const {
+    AdsAddGetController,
+    AdsAddPostController,
+    AdsGetOneController,
+} = require("../controllers/AdsRouteController");
+const fileUpload = require("express-fileupload");
 
 const router = require("express").Router();
 
-router.get("/add", AdsGetController);
+router.get("/add", AdsAddGetController);
+router.post("/add", fileUpload(), AdsAddPostController);
+router.get("/:slug", AdsGetOneController);
 
 module.exports = {
     path: "/ads",
